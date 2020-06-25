2d080b84decdaea4c45ca0c459a72a7117aee125
Logan Blackstad <logan.blackstad@gmail.com>
2020-06-25 11:18:03 -0400

    refactored for initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './App.css';
-// import MovieCard from './components/MovieCard';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import MovieList from './components/MovieList';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import SearchPage from './components/SearchPage';
+
+
+// set up react router --> look @ code kitchen
+
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route path="/" exact component={MovieList} />
-          {/* <Route path="/movies/:id" component={MovieCard} /> */}
+          <Route path='/' exact component={SearchPage} />
+          <Route path='/movies/:id' component={MovieList} />
         </Switch>
       </div>
     </BrowserRouter>
+
   );
 }
 
